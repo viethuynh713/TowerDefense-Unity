d20b583152e242aa23309db5774d11b826d91400
viethuynh713 <66113968+viethuynh713@users.noreply.github.com>
2023-07-26 00:03:53 +0700

    Update Rocket tower

--- Assets/Scripts/InGame/Bullet/RocketBullet.cs
+++ Assets/Scripts/InGame/Bullet/RocketBullet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace MythicEmpire.InGame
 {
@@ -8,16 +9,20 @@ namespace MythicEmpire.InGame
     {
         private List<Vector3> waypoints;
         private int currentWaypointIndex = 0;
+        private float curveSpeed = 2f; // Tốc độ di chuyển trên đường cong
+        private float t = 0f; // Biến thời gian dùng cho hàm Bezier
         private void Start()
         {
+            Vector3 direction = (target.transform.position - transform.position).normalized;
+            var numOfPoint = Random.Range(4, 8);
             waypoints = new List<Vector3>()
             {
-                transform.position + Vector3.up,
-                transform.position + Vector3.left,
-                transform.position + Vector3.right*2,
-                transform.position + Vector3.down,
-                transform.position + Vector3.left + Vector3.one,
+                transform.position + Vector3.forward,
             };
+            for (int i = 1; i < numOfPoint; i++)
+            {
+                waypoints.Add(waypoints[0]+direction*Random.Range(0,0.2f));
+            }
         }
 
         public override void Move()
@@ -29,18 +34,29 @@ namespace MythicEmpire.InGame
             }
 
             
-            if (currentWaypointIndex < waypoints.Count)
+            if (currentWaypointIndex < waypoints.Count - 4)
             {
-                transform.LookAt(waypoints[currentWaypointIndex]);
-                
-                transform.position = Vector3.MoveTowards(transform.position, 
-                    waypoints[currentWaypointIndex], 
-                    bulletSpeed * Time.deltaTime);
+                // Tính toán vị trí trên đường cong Bézier
+                Vector3 p0 = waypoints[currentWaypointIndex];
+                Vector3 p1 = waypoints[currentWaypointIndex + 1];
+                Vector3 p2 = waypoints[currentWaypointIndex + 2];
+                Vector3 p3 = waypoints[currentWaypointIndex + 3];
 
-                // Kiểm tra xem đã đến waypoint trung gian hiện tại chưa
-                if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex]) < 0.1f)
+                Vector3 positionOnCurve = CalculateBezierPoint(t, p0, p1, p2, p3);
+
+                transform.LookAt(positionOnCurve);
+
+                transform.position = Vector3.MoveTowards(transform.position,
+                    positionOnCurve,
+                    bulletSpeed * curveSpeed * Time.deltaTime);
+
+                t += Time.deltaTime / Vector3.Distance(p0, p1) * curveSpeed;
+
+                // Kiểm tra xem đã đi qua waypoint trung gian hiện tại chưa
+                if (t >= 1f)
                 {
                     currentWaypointIndex++;
+                    t = 0f;
                 }
             }
             else
@@ -51,12 +67,26 @@ namespace MythicEmpire.InGame
                     target.transform.position, 
                     bulletSpeed * Time.deltaTime);
                 
-                if (Vector3.Distance(transform.position,
-                        new Vector3(transform.position.x, transform.position.y,target.transform.position.z)) < InGameService.infinitesimal)
+                if (Vector3.Distance(transform.position,target.transform.position) < InGameService.infinitesimal)
                 {
                     Explore();
                 }
             }
+        }
+        private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            float u = 1f - t;
+            float tt = t * t;
+            float uu = u * u;
+            float uuu = uu * u;
+            float ttt = tt * t;
+
+            Vector3 p = uuu * p0; // (1-t)^3 * P0
+            p += 3f * uu * t * p1; // 3 * (1-t)^2 * t * P1
+            p += 3f * u * tt * p2; // 3 * (1-t) * t^2 * P2
+            p += ttt * p3; // t^3 * P3
+
+            return p;
         }
     }
     
--- Assets/Scripts/Networking System/NetworkingConfig.cs
+++ Assets/Scripts/Networking System/NetworkingConfig.cs
@@ -4,6 +4,7 @@
     {
         public string ServiceURL = "https://port7dedj9.execute-api.ap-southeast-1.amazonaws.com/api/";
         public string RealtimeURL = "http://localhost:5190";
-        // public string RealtimeURL = "https://1ee9-171-252-125-219.ngrok-free.app";
+        // public string RealtimeURL = "https://4gg4ezxr2g.execute-api.ap-southeast-1.amazonaws.com";
+        
     }
 }
