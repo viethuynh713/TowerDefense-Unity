355cc850b2e716c0ca430e782c6c984da5b36b0d
viethuynh713 <66113968+viethuynh713@users.noreply.github.com>
2023-07-25 16:45:05 +0700

    Update fireball tower

--- Assets/Scripts/InGame/Bullet/FireBallBullet.cs
+++ Assets/Scripts/InGame/Bullet/FireBallBullet.cs
@@ -5,57 +5,65 @@ namespace MythicEmpire.InGame
 {
     public class FireBallBullet : ZoneBullet
     {
-        public float height = 2f;
-        private Vector3 startPos;
-        private float duration;
+        public float height = 5f;
+        
+        private float angle = 0f; // Góc di chuyển của viên đạn theo hình vòng
+        private Vector3 initialPosition; // Vị trí ban đầu của viên đạn
+        private Vector3 targetPosition; // Vị trí của đối thủ
+        private Vector3 direction; // Hướng di chuyển của viên đạn
+        private float flightDuration; // Thời gian bay từ vị trí ban đầu đến độ cao y=3
+        private float startTime; // Thời điểm bắt đầu bay từ vị trí ban đầu
+
         private void Start()
         {
-            startPos = transform.position;
+            
+            initialPosition = transform.position; // Lưu vị trí ban đầu của viên đạn
+            targetPosition = target.transform.position; // Lưu vị trí của đối thủ
 
+            // Tính toán hướng di chuyển từ vị trí ban đầu của viên đạn đến đối thủ
+            direction = (targetPosition - initialPosition).normalized;
+
+            // Tính toán thời gian bay từ vị trí ban đầu đến độ cao y=3
+            float distanceY = targetPosition.y - initialPosition.y;
+            flightDuration = Mathf.Max(0.1f, Mathf.Abs(distanceY / height));
+
+            // Lưu thời điểm bắt đầu bay
+            startTime = Time.time;
         }
 
         public override void Move()
         {
-            base.Move();
-            // if (target == null)
-            // {
-            //     Explore();
-            // }
-            //
-            // else
-            // {
-            //     transform.LookAt(target.transform.position);
-            //     // Calculate the distance between position A and position B
-            //     float distance = Vector3.Distance(startPos, target.transform.position);
-            //
-            //     // Calculate the duration based on the distance and the speed
-            //     float duration = distance / bulletSpeed;
-            //
-            //     // Increment the time elapsed since the movement started
-            //     float timeElapsed = Mathf.Clamp(Time.deltaTime, 0f, duration);
-            //
-            //     // Calculate the normalized time (0 to 1) based on the elapsed time and duration
-            //     float normalizedTime = timeElapsed / duration;
-            //
-            //     // Calculate the vertical position using a parabolic equation (y = a*x^2 + b*x + c)
-            //     float verticalOffset = height * 4f * (normalizedTime - 0.5f) * (normalizedTime - 0.5f);
-            //
-            //     // Calculate the horizontal position
-            //     Vector3 horizontalOffset = Vector3.Lerp(startPos, target.transform.position, normalizedTime);
-            //
-            //     // Calculate the final position with both vertical and horizontal offsets
-            //     Vector3 finalPosition = horizontalOffset + Vector3.up * verticalOffset;
-            //
-            //     // Update the object's position
-            //     transform.position = finalPosition;
-            //
-            //     // Check if the movement is complete
-            //     if (normalizedTime >= 1f)
-            //     {
-            //         Explore();
-            //     }
-            }
-        
 
+            if (target == null)
+            {
+                Explore();
+            }
+
+            else
+            {
+                transform.LookAt(target.transform.position);
+                
+                float timeSinceStart = Time.time - startTime;
+
+                // Tính toán tỷ lệ thời gian đã bay so với thời gian bay tổng cộng
+                float normalizedTime = Mathf.Clamp01(timeSinceStart / flightDuration);
+
+                // Tính toán vị trí mới của viên đạn dựa trên đường parabol
+                float heightOffset = height * (normalizedTime - normalizedTime * normalizedTime);
+                Vector3 newPosition = Vector3.Lerp(initialPosition, targetPosition, normalizedTime) + Vector3.up * heightOffset;
+
+                // Di chuyển viên đạn tới vị trí mới
+                transform.position = newPosition;
+
+                if (Vector3.Distance(transform.position,
+                        new Vector3(transform.position.x, transform.position.y,target.transform.position.z)) < InGameService.infinitesimal 
+                    ||transform.position.y == target.transform.position.y )
+                {
+                    Explore();
+                }
+            }
+
+
+        }
     }
 }
---
+++ Assets/Scripts/InGame/Bullet/RocketBullet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MythicEmpire.InGame
+{
+    public class RocketBullet : ZoneBullet
+    {
+        private List<Vector3> waypoints;
+        private int currentWaypointIndex = 0;
+        private void Start()
+        {
+            waypoints = new List<Vector3>()
+            {
+                transform.position + Vector3.up,
+                transform.position + Vector3.left,
+                transform.position + Vector3.right*2,
+                transform.position + Vector3.down,
+                transform.position + Vector3.left + Vector3.one,
+            };
+        }
+
+        public override void Move()
+        {
+            if (target == null)
+            {
+                Explore();
+                return;
+            }
+
+            
+            if (currentWaypointIndex < waypoints.Count)
+            {
+                transform.LookAt(waypoints[currentWaypointIndex]);
+                
+                transform.position = Vector3.MoveTowards(transform.position, 
+                    waypoints[currentWaypointIndex], 
+                    bulletSpeed * Time.deltaTime);
+
+                // Kiểm tra xem đã đến waypoint trung gian hiện tại chưa
+                if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex]) < 0.1f)
+                {
+                    currentWaypointIndex++;
+                }
+            }
+            else
+            {
+                transform.LookAt(target.transform.position);
+                
+                transform.position = Vector3.MoveTowards(transform.position, 
+                    target.transform.position, 
+                    bulletSpeed * Time.deltaTime);
+                
+                if (Vector3.Distance(transform.position,
+                        new Vector3(transform.position.x, transform.position.y,target.transform.position.z)) < InGameService.infinitesimal)
+                {
+                    Explore();
+                }
+            }
+        }
+    }
+    
+}
