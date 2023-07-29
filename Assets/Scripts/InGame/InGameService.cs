97a9e064f1e45b47b3be3db487e3a871dc0e8e32
Khánh <77840594+sshinichi-69@users.noreply.github.com>
2023-07-30 03:47:18 +0700

    change find node order in find path

--- Assets/Scripts/InGame/InGameService.cs
+++ Assets/Scripts/InGame/InGameService.cs
@@ -92,10 +92,10 @@ namespace MythicEmpire.InGame
             else
             {
                 possibleTiles = new List<FPTile>() {
-                    new FPTile { x = currentTile.x, y = currentTile.y - 1, parent = currentTile, cost = currentTile.cost + 1 },
-                    new FPTile { x = currentTile.x, y = currentTile.y + 1, parent = currentTile, cost = currentTile.cost + 1 },
                     new FPTile { x = currentTile.x - 1, y = currentTile.y, parent = currentTile, cost = currentTile.cost + 1 },
-                    new FPTile { x = currentTile.x + 1, y = currentTile.y, parent = currentTile, cost = currentTile.cost + 1 }
+                    new FPTile { x = currentTile.x + 1, y = currentTile.y, parent = currentTile, cost = currentTile.cost + 1 },
+                    new FPTile { x = currentTile.x, y = currentTile.y - 1, parent = currentTile, cost = currentTile.cost + 1 },
+                    new FPTile { x = currentTile.x, y = currentTile.y + 1, parent = currentTile, cost = currentTile.cost + 1 }
                 };
             }
 
