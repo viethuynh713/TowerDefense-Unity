6dd99b34d2535526209fdb9a8ee3df9c3a100cb3
Khánh <77840594+sshinichi-69@users.noreply.github.com>
2023-10-11 13:25:18 +0700

    push

--- Assets/Scripts/Networking System/NetworkingConfig.cs
+++ Assets/Scripts/Networking System/NetworkingConfig.cs
@@ -3,8 +3,8 @@
     public class NetworkingConfig
     {
         public string ServiceURL = "https://port7dedj9.execute-api.ap-southeast-1.amazonaws.com/api/";
-        // public string RealtimeURL = "http://localhost:5190";
-        public string RealtimeURL = "http://13.215.117.233:5000/";
-        
+        //public string RealtimeURL = "https://localhost:7190";
+        //public string RealtimeURL = "http://13.215.117.233:5000/";
+        public string RealtimeURL = "https://54.254.12.16:5000/";
     }
 }
